45a5c17370b9b0e235b1c4d85174959ae85804e6
olamijinadebayo <consultolamijin@gmail.com>
2018-04-01 23:53:37 +0300

    this will add the class to enable us instantiate a new class

--- user.py
+++ user.py
@@ -1,1 +1,8 @@
-class User
+class User():
+    """
+     class which will generate new instances for every new user
+    """
+    user_list = []
+    def __init__(self,user_name,password):
+        self.user_name = user_name
+        self.password = password
