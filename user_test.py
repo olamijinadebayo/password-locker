bf00d0d546f34bf61eaa7ef34d6b18abf029d9c3
olamijinadebayo <consultolamijin@gmail.com>
2018-04-02 00:04:11 +0300

    this will import unittest and ensure that each user instance gives the required result

--- user_test.py
+++ user_test.py
@@ -1,1 +1,23 @@
-import unittest
+import unittest # importing the unittest module
+from user import User # Importing the contact class
+import pyperclip
+
+class TestUser(unittest.TestCase):
+
+
+    def setUp(self):
+        '''
+        setup up method to run before each test cases.
+        '''
+        self.new_user = User("Olamijin","09037465697") # create contact oblect
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_user.user_name,"Olamijin")
+        self.assertEqual(self.new_user.password,"09037465697")
+
+if __name__ == '__main__':
+    unittest.main()
