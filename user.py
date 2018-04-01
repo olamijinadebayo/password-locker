51f322ba4cc1b4901f0e07874fc64835adeaf3ee
olamijinadebayo <consultolamijin@gmail.com>
2018-04-02 00:15:59 +0300

    this will add a function to save the user login details for every instance

--- user.py
+++ user.py
@@ -6,3 +6,9 @@ class User():
     def __init__(self,user_name,password):
         self.user_name = user_name
         self.password = password
+
+    def save_user(self):
+        """
+        function which will append every instance of the user to the user list
+        """
+        User.user_list.append(self)
