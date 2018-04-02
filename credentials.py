9c1d36fc56216ce7a92d1b5646ba7bb6f0b805ec
olamijinadebayo <consultolamijin@gmail.com>
2018-04-02 15:24:15 +0300

    this will add the function which will enable user to delete and find credentials

--- credentials.py
+++ credentials.py
@@ -11,3 +11,18 @@ class Credential(object):
 
     def save_credential(self):
         Credential.credential_list.append(self)
+
+    def delete_credential(self):
+        '''
+        remove credential objects from credential list
+        '''
+        Credential.credential_list.remove(self)
+
+    @classmethod
+    def find_by_username(cls, name):
+        '''
+        method takes in the user name and returns a credential that matches it
+        '''
+        for credential in cls.credential_list:
+            if credential.user_name == name:
+                return credential
