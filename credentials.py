84a6aa02656003ebb21b77b1fa905849b57b91f9
olamijinadebayo <consultolamijin@gmail.com>
2018-04-02 15:21:11 +0300

    this will add a function to test if we can search for password using the username

--- credentials.py
+++ credentials.py
@@ -1,11 +1,13 @@
 
 class Credential(object):
-    """docstring for Credential."""
-    credential_list[]
+    """
+    docstring for Credential.
+    """
+    credential_list = []
     def __init__(self,account,user_name,password):
         self.account = account
         self.user_name = user_name
         self.password = password
 
     def save_credential(self):
-        credential_list.append(self)
+        Credential.credential_list.append(self)
--- credentials_test.py
+++ credentials_test.py
@@ -1,5 +1,5 @@
 import unittest # importing the unittest module
-from credential import Credential # Importing the contact class
+from credentials import Credential # Importing the contact class
 import pyperclip
 
 class TestCredential(unittest.TestCase):
@@ -9,7 +9,7 @@ class TestCredential(unittest.TestCase):
         '''
         setup up method to run before each test cases.
         '''
-        self.new_credential = Credential("pinterest","Olamijin""09037465697") # create contact oblect
+        self.new_credential = Credential("pinterest","Olamijin","09037465697") # create contact oblect
 
     def test_init(self):
         '''
@@ -32,3 +32,29 @@ class TestCredential(unittest.TestCase):
         tearDown method that does clean up after each test case has run.
         '''
         Credential.credential_list = []
+
+    def test_delete_credential(self):
+        '''
+        test to see if a credential can be deleted
+        '''
+        self.new_credential.save_credential()
+        test_credential = Credential("pinterest", "olamijin", "09037465697") # new credentials
+        test_credential.save_credential()
+
+        self.new_credential.delete_credential() # delete credential object
+        self.assertEqual(len(Credential.credential_list), 1)
+
+    def test_find_password_by_username(self):
+        '''
+        test to see if we can find a password using the username
+        '''
+        self.new_credential.save_credential()
+        test_credential = Credential("pinterest", "olamijin", "09037465697")
+        test_credential.save_credential()
+        found_credential = Credential.find_by_username("olamijin")
+
+        self.assertEqual(found_credential.password, test_credential.password)
+
+
+if __name__ == '__main__':
+    unittest.main()
