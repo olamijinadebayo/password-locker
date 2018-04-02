5bcee5bc9c5bed866303a5a668178852b5458f98
olamijinadebayo <consultolamijin@gmail.com>
2018-04-02 16:55:58 +0300

    this will add a uuid module which is used in writing the function used to generate password

--- user.py
+++ user.py
@@ -1,3 +1,4 @@
+import uuid
 class User():
     """
      class which will generate new instances for every new user
@@ -13,7 +14,7 @@ class User():
         """
         User.user_list.append(self)
     @classmethod
-    def user_exist(cls, name, password):
+    def user_exist(cls, name,password):
         '''
         authenticate user if they have an account
         '''
@@ -21,3 +22,22 @@ class User():
             if user.user_name == name and user.password == password:
                 return user
         return False
+
+    # def generate_password():
+    #     '''
+    #     function to generate a new password
+    #     '''
+    #
+    #     alphabet = string.ascii_letters + string.digits
+    #     password = ''.join(secrets.choice(alphabet) for i in range(5))
+    #
+    #     return password
+
+    def random_password(string_length=10):
+        """
+        Returns a random string of length string_length.
+        """
+        password = str(uuid.uuid4()) # Convert UUID format to a Python string.
+        password = password.upper() # Make all characters uppercase.
+        password = password.replace("-","") # Remove the UUID '-'.
+        return password[0:string_length] # Return the random string.
